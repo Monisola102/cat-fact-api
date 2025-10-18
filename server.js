8cddea44c3cdc422367b9fa43215d9ca17d561e7
Monisola102 <Oyewolemonisola102@gmail.com>
2025-10-18 07:06:38 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 const app = express();
 //redeploy test
 
